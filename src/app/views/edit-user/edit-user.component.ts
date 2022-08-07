a16793c7b71855cf26061d3be84e25697997b8b1
Robert Veinberg-Vikking <robivikking98@hotmail.com>
2022-08-07 12:52:29 -0400

    isVolunteer dynamically displays fields for volunteer entry

--- src/app/views/edit-user/edit-user.component.ts
+++ src/app/views/edit-user/edit-user.component.ts
@@ -54,18 +54,6 @@ export class EditUserComponent implements OnInit {
 
   userId: number | null = null;
 
-  isShowingAddPrefference: boolean = false;
-  isShowingAddAvailabilityTimes: boolean = false;
-  isShowingAddSkillsOrInterests: boolean = false;
-  isShowingAddPhoneNumbers: boolean = false;
-  isShowingAddCurrentLicenses: boolean = false;
-  
-  addedPreferrence: string | null = null;
-  addedSkillOrInterest: string | null = null;
-  addedAvailabilityTime: string | null = null;
-  addedPhoneNumber: string | null = null;
-  addedCurrentLicense: string | null = null;
-
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((p) => {
       this.userId = p['userId'];
@@ -87,109 +75,8 @@ export class EditUserComponent implements OnInit {
     }
   }
 
-  setIsShowingAddPreferrences(flag: boolean) {
-    this.isShowingAddPrefference = flag;
-  }
-
-  setIsShowingAddAvailabilityTimes(flag: boolean) {
-    this.isShowingAddAvailabilityTimes = flag;
-  }
-
-  setIsShowingAddSkillsOrInterests(flag: boolean) {
-    this.isShowingAddSkillsOrInterests = flag;
-  }
-
-  setIsShowingAddPhoneNumbers(flag: boolean) {
-    this.isShowingAddPhoneNumbers = flag;
-  }
-
-  setIsShowingAddCurrentLicenses(flag: boolean) {
-    this.isShowingAddCurrentLicenses = flag;
-  }
-
-  CreatePreferrence() {
-    if (this.addedPreferrence && this.addedPreferrence.length > 0) {
-      if(!this.preferredWorkCenters) {
-        this.preferredWorkCenters = [];
-      }
-      this.preferredWorkCenters.push(this.addedPreferrence);
-      this.addedPreferrence = null;
-      this.setIsShowingAddPreferrences(false);
-    }
-  }
-
-  DeletePreferrence(itemToDelete: string) {
-    if(this.preferredWorkCenters) {
-      this.preferredWorkCenters = this.preferredWorkCenters.filter(item => item !== itemToDelete);
-    }
-  }
-
-  CreateAvailabilityTime() {
-    if (this.addedAvailabilityTime && this.addedAvailabilityTime.length > 0) {
-      if(!this.availabilityTimes) {
-        this.availabilityTimes = [];
-      }
-      this.availabilityTimes.push(this.addedAvailabilityTime);
-      this.addedPreferrence = null;
-      this.setIsShowingAddAvailabilityTimes(false);
-    }
-  }
-
-  DeleteAvailabilityTime(itemToDelete: string) {
-    if(this.availabilityTimes) {
-      this.availabilityTimes = this.availabilityTimes.filter(item => item !== itemToDelete);
-    }
-  }
-
-  CreateSkillOrInterest() {
-    if (this.addedSkillOrInterest && this.addedSkillOrInterest.length > 0) {
-      if(!this.skillsOrInterests) {
-        this.skillsOrInterests = [];
-      }
-      this.skillsOrInterests.push(this.addedSkillOrInterest);
-      this.addedPreferrence = null;
-      this.setIsShowingAddPreferrences(false);
-    }
-  }
-
-  DeleteSkillOrInterest(itemToDelete: string) {
-    if(this.skillsOrInterests) {
-      this.skillsOrInterests = this.skillsOrInterests.filter(item => item !== itemToDelete);
-    }
-  }
-
-  CreatePhoneNumber() {
-    if (this.addedPhoneNumber && this.addedPhoneNumber.length > 0) {
-      if(!this.phoneNumbers) {
-        this.phoneNumbers = [];
-      }
-      this.phoneNumbers.push(this.addedPhoneNumber);
-      this.addedPreferrence = null;
-      this.setIsShowingAddPreferrences(false);
-    }
-  }
-
-  DeletePhoneNumber(itemToDelete: string) {
-    if(this.phoneNumbers) {
-      this.phoneNumbers = this.phoneNumbers.filter(item => item !== itemToDelete);
-    }
-  }
-
-  CreateCurrentLicense() {
-    if (this.addedCurrentLicense && this.addedCurrentLicense.length > 0) {
-      if(!this.currentLicenses) {
-        this.currentLicenses = [];
-      }
-      this.currentLicenses.push(this.addedCurrentLicense);
-      this.addedPreferrence = null;
-      this.setIsShowingAddPreferrences(false);
-    }
-  }
-
-  DeleteCurrentLicense(itemToDelete: string) {
-    if(this.currentLicenses) {
-      this.currentLicenses = this.currentLicenses.filter(item => item !== itemToDelete);
-    }
+  setIsVolunteer(flag: boolean) {
+    this.isVolunteer = !flag;
   }
 
 }
